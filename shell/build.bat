c9d1c75836aca54236a1eee694eb73fb25096d96
Leon Bass <leonandersonbass@gmail.com>
2024-06-30 14:12:21 +0100

    fixed up build output executable name to remove "win" since this is implied

--- shell/build.bat
+++ shell/build.bat
@@ -10,4 +10,4 @@ cd ..
 call ./shell/install_requirements.bat
 call ./shell/build_seed_changer.bat
 
-nuitka --follow-imports --output-dir=dist src/main.py --onefile --output-filename=noita_seed_tool_winx64_v1.0.0.exe
+nuitka --follow-imports --output-dir=dist src/main.py --onefile --output-filename=noita_seed_tool_x64_v1.0.0.exe
