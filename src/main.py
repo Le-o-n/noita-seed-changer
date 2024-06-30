fbc5e31c8fff00451b7df7b45cb564c7c6f6f758
Leon Bass <leonandersonbass@gmail.com>
2024-06-30 14:25:57 +0100

    updated to include GNU v3 license

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
+"""
+    Noita Seed Changer - A tool for changing the seed of the video game
+    "Noita" using runtime modifications the game's virtual memory.
+    Copyright (C) 2024  Leon Bass (https://github.com/Le-o-n)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from seed_changer.seed_changer import main
 
 main()
